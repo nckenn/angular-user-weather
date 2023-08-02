8ca7222e46f88e1563c5ad18a0b099108f2291b8
nckenn <nckenn.developer@gmail.com>
2023-08-02 10:18:41 +0800

    chore: added tailwind css for styling

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}"
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
